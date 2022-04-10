3b8636387f9c4d7b323f2762bbc88ea58515d959
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-04-10 14:34:21 -0700

    python escape

---
+++ escape.py
@@ -0,0 +1,22 @@
+sString = " this has been \nsplit over\nseveral\nlines "
+print(sString)
+
+tString = "1\t2\t3\t5"
+print(tString)
+
+print ('the pet shop ownser said "NO, no, \'e\'s uh,...he\'s resting".')
+print ("the pet shp ownser said \"No, no , 'e's uh,... he's resting\".")
+
+print ("""the pet ship owner said" No, no, 'e's uh,.. he's resting". """)
+
+aSplit = """This string has been
+
+split 
+\
+over 
+
+severl 
+\
+lines """
+
+print( aSplit)
