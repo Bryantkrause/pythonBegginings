3465b5bec61b55e87a68da4b44f910bfc4515140
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-04-10 19:56:51 -0700

    I got strings for daaaaaaaays

--- printingtabs.py
+++ printingtabs.py
@@ -2,4 +2,5 @@ a = "Number 1\tThe Larch\nNumber 2\tThe Horse Chestnut"
 
 print(a)
 
-
+print("C:Users\\bryant\\Desktop\\rebirth\\pythonBeginings\\notes.txt")
+print(r"C:Users\bryant\Desktop\rebirth\pythonBeginings\notes.txt")
