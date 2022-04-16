5ce3c82db259ab5e1f7d8f18b5e5aff246284b1c
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-04-16 08:36:15 -0700

    expressions

---
+++ builtin.py
@@ -0,0 +1,26 @@
+# integer datat type is int
+
+# float has decimal up to 308 places
+
+
+first = 12
+
+second = 3
+
+print(first + second)
+print(first - second)
+print(first * second)
+print(first / second)
+print(first // second)  # this is for integer no decimals
+print(first % second)
+
+print()
+
+# first for loop in python
+for i in range(1, 4):
+    print(i)
+
+bun_price = 2.40
+money = 15
+
+print(money // bun_price)
