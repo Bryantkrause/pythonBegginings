fc742de31ed794e31aed6f481027b3841da91879
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-04-22 07:39:36 -0700

    string indexing

---
+++ str.py
@@ -0,0 +1,9 @@
+'string is a sequence'
+
+parrot = "Norwegian Blue"
+
+
+print(parrot)
+print(parrot[3])
+print(parrot[3], "\n", parrot[4], "\n", "\n", parrot[3],
+      "\n", parrot[6], "\n", parrot[8], "\n",)
