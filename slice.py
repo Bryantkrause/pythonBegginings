5947cd23794798cf56691dd0f88a519485a8b87e
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-04-30 06:32:23 -0700

    slice

---
+++ slice.py
@@ -0,0 +1,9 @@
+parrot = "Norwegian Blue"
+
+print(parrot[0:6])
+
+'up to but not including slice does not use last position of index'
+
+print(parrot[:6]) #'defaults to start value'
+print(parrot[6:])  # 'defaults to end value'
+print(parrot[:]) #just does everything
