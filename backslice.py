8b61f8b5a597386b1a1345a0b3f1948b62c33bf1
bryantkrause <bryantkrause@gmail.com>
2022-07-01 11:21:34 -0700

    I am a string master

--- backslice.py
+++ backslice.py
@@ -6,3 +6,17 @@ backwards3 = letters[::-1] #reverse whole string || reverses entire string no ma
 print(backwards)
 print(backwards2)
 print(backwards3)
+challenge1 = letters[16:13:-1] 
+print(challenge1)
+challenge2 = letters[4::-1] 
+print(challenge2)
+challenge3 = letters[25:25-8:-1] 
+print(challenge3)
+
+pythonIdiom = letters[::-1] # this is for reversing string
+
+print(letters[-1:]) # this is for getting last digit
+
+print(letters[:1]) # this is for getting first digit
+
+print(letters[0]) # this gets first digit however if string is empty will result in error
