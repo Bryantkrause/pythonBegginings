f0d93abd30348cad6eb30ba6ab1dc05e039f079a
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-07-02 10:48:50 -0700

    replacement

---
+++ repfields.py
@@ -0,0 +1,11 @@
+age = 24
+
+print("My age is " +str(age) + " years")
+
+print("My age is {0} years".format(age+1))
+
+print("There are {0} days in {1}, {2},{3},{4}"
+    .format(31, "JAN", "Mar", "May", "Jul"))
+
+print("There are {2} days in {4}, {2},{3},{2}"
+      .format(31, "JAN", "Mar", "May", "Jul"))
