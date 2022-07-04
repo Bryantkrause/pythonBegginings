8babd3a47c41a875f455c335e87449feb2d83c64
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-07-04 07:11:02 -0700

    what is it good FOR

--- formatting.py
+++ formatting.py
@@ -1,10 +1,12 @@
 for i in range(1, 13):
-    print("No. {0:2} squared is {1:3} and cubed is {2:4}".format(i, i ** 2, i ** 3)) # this use of format reserves that number of columns for space so a {0:2} reservers 2 columns
+    # this use of format reserves that number of columns for space so a {0:2} reservers 2 columns
+    print("No. {0:2} squared is {1:3} and cubed is {2:4}".format(
+        i, i ** 2, i ** 3))
 
 for i in range(1, 13):
     print("No. {0:2} squared is {1:<3} and cubed is {2:^4}".format(
         i, i ** 2, i ** 3
-        )) # use <> operators to choose alingment type 
+    ))  # use <> operators to choose alingment type
 
 print("Pi is aprox {0:12}".format(22/7))
 print("Pi is aprox {0:12f}".format(22/7))
@@ -14,4 +16,21 @@ print("Pi is aprox {0:62.50f}".format(22/7))
 pi = 22/7
 print("Pi is aprox {0:72.50f}".format(22/7))
 print(f"Pi is aprox {pi:12.50f}")
-#who needs .format when you can just f it
+# who needs .format when you can just f it
+
+meal2 = "spam\neggs\nbeans"
+meal1 = """spam
+eggs
+beans"""
+
+print(meal2)
+print(meal1)
+
+a = 45
+b = 15
+c = 3
+
+print(a - b / c)
+
+days = "Mon, Tue, Wed, Thu, Fri, Sat, Sun"
+print(days[::5])
