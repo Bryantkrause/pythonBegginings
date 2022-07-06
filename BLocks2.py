bf197513c254ecc21ae3cc7f93ca771b792eb659
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-07-05 21:16:37 -0700

    odd and even loop

--- BLocks2.py
+++ BLocks2.py
@@ -2,14 +2,16 @@ name = input("Please enter your name: ")
 age = int(input(f"How old are you, {name}? "))
 print(age)
 
-if age >= 18:
-    print("You are old enough to vote")
-    print("Please put an X in the box")
-else:
-    print(f"Please come back in {18 - age} years.")
+# if age >= 18:
+#     print("You are old enough to vote")
+#     print("Please put an X in the box")
+# else:
+#     print(f"Please come back in {18 - age} years.")
 
 if age < 18:
     print(f"Please come back in {18 - age} years.")
+elif age == 900:
+    print("thats not a real age")
 else:
     print("You are old enough to vote")
     print("Please put an X in the box")
--- Blocks.py
+++ Blocks.py
@@ -1,3 +1,15 @@
-for i in range(1, 13):
+start = 1
+
+end = 100
+
+
+
+for i in range(1, 2):
     print(f"No. {i:2} squared is {i ** 2:3} and cubed is {i ** 3:4}")
     print("*" * 80)
+
+for i in range(start, end):
+    if i % 2 == 0:
+        print(f"{i} is even")
+    else:
+        print(f"{i} is odd")
