362a34375da8f2600ac6b1801310e5984301e26b
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-07-07 18:15:10 -0700

    code exercise if

---
+++ codeex5.py
@@ -0,0 +1,17 @@
+tree1 = 'oak'
+tree2 = 'oak'
+
+if tree1 == tree2:
+    print("The trees are the same")
+else:
+    print("the trees are different")
+
+x = 5
+y = 7
+
+if x == y:
+    print("x = y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is smaller than y")
