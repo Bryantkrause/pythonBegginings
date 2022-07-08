5e9181e3a9afee516cbfb5bd01c389317b825384
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-07-07 18:21:57 -0700

    big more guesses

--- guessinggame.py
+++ guessinggame.py
@@ -6,7 +6,17 @@ guess = int(input())
 
 if guess < answer:
     print("Please guess higher")
+    guess = int(input())
+    if guess == answer:
+        print("Well done you got it")
+    else:
+        print("Sorry, you have guessed incorrectly")
 elif guess > answer:
     print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done you got it")
+    else:
+        print("Sorry, you have guessed incorrectly")
 else:
     print("you got it first time")
