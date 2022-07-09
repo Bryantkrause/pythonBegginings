bdb0029b1a5846a8fb55bcc20333527c38f17d72
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-07-09 11:52:16 -0700

    get that invoice number I think i should try removing all spaces

--- Pdfinator/PDFR.py
+++ Pdfinator/PDFR.py
@@ -1,7 +1,12 @@
 import PyPDF2
 import csv
+import os
+
+path = r"C:\Users\bryan\Desktop\rebirth\pythonBeginings\Pdfinator"
+dirs = os.listdir(path)
 # creating a pdf file object
-pdfFileObj = open(r"C:\Users\bryan\Desktop\rebirth\pythonBeginings\Pdfinator\Files\36907.pdf", 'rb')
+pdfFileObj = open(
+    r"C:\Users\bryan\Desktop\rebirth\pythonBeginings\Pdfinator\Files\36907.pdf", 'rb')
 # creating a pdf reader object
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 # printing number of pages in pdf file
@@ -13,14 +18,35 @@ print(pageObj.extractText())
 # closing the pdf file object
 pdfFileObj.close()
 
-# open the file in the write mode
-f = open(r'C:\Users\bryan\Desktop\rebirth\pythonBeginings\Pdfinator\csv_file', 'w')
+invoiceDetail = []
+# # open the file in the write mode
+f = open(r'C:\Users\bryan\Desktop\rebirth\pythonBeginings\Pdfinator\output.csv', 'w')
 
-# create the csv writer
+# # create the csv writer
 writer = csv.writer(f)
 
-# write a row to the csv file
-writer.writerow(pageObj.extractText())
+# # write a row to the csv file
+# writer.writerow(pageObj.extractText())
 
-# close the file
+# # close the file
 f.close()
+
+
+# find start of invoice #
+
+
+for file in dirs:
+    print(file)
+
+with open(r"C:\Users\bryan\Desktop\rebirth\pythonBeginings\Pdfinator\csv_file.csv") as x:
+    reader = csv.reader(x)
+    with open(r'C:\Users\bryan\Desktop\rebirth\pythonBeginings\Pdfinator\output.csv', 'w') as g:
+        writer = csv.writer(g)
+        for row in reader:
+            new_row = [' '.join(row)]
+            writer.writerow(new_row)
+            invoiceNumber = ' '.join(new_row)
+            # print(invoiceNumber)
+            invoiceFinder = invoiceNumber.find('I n v o i c e   #')
+            print(invoiceNumber.find('3 6 9 0 7'))
+            print(invoiceNumber[invoiceFinder+20:invoiceFinder+30])
