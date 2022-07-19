9a1342be69d2c9b6e31ce7a2f41dde49a77e7d8f
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-07-18 21:29:45 -0700

    writing numbers that arent numbers

--- save_flowers.py
+++ save_flowers.py
@@ -20,8 +20,36 @@ data = [
     "Witch Hazel - Shrub",
 ]
 
-plantsFile = "flowers_print.txt"
+# plantsFile = "flowers_print.txt"
 
-with open(plantsFile, "w") as plants:
-    for plant in data:
-        print(plant, file=plants)
+# with open(plantsFile, "w") as plants:
+#     for plant in data:
+#         print(plant, file=plants)
+
+# newList = []
+
+# with open(plantsFile) as plants:
+#     for plant in plants:
+#         newList.append(plant.rstrip())
+
+# print(newList)
+
+# plantsFile = "flowers_write.txt"
+
+# with open(plantsFile, "w") as planter:
+#     for plant in data:
+#         planter.write(plant)
+
+# print(data)
+# string_rep = data.__str__()
+# print(type(string_rep))
+
+name = "test_number.txt"
+with open(name, "w") as test:
+    for i in range(10):
+        print(i, file=test)
+
+name = "test_number.txt"
+with open(name, "w") as test:
+    for i in range(10):
+        test.write(str(i) + "\n")
