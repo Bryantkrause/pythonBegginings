4786ede6949d236a273403b81c1726799876c5b9
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-07-23 15:20:51 -0700

    ride the dictionary

--- countryChallenge.py
+++ countryChallenge.py
@@ -4,21 +4,29 @@ import csv
 codeLookUp = []
 fileName = 'country_info.txt'
 
+dialect = csv.excel
+dialect.delimiter = '|'
+
 countries = {}
 with open(fileName, encoding='utf-8', newline='') as countryFile:
-    dictReader = csv.DictReader(countryFile, delimiter='|')
+    # get headings
+    headings = countryFile.readline().strip('\n').split(dialect.delimiter)
+    for index, heading in enumerate(headings):
+        headings[index] = heading.casefold()
+
+    dictReader = csv.DictReader(countryFile, dialect=dialect, fieldnames=headings)
     for row in dictReader:
         # countries[country.casefold()] = country_dict
       
-        countries[row['Country'].casefold()] = row
+        countries[row['country'].casefold()] = row
         # countries[code.casefold()] = country_dict
-        countries[row['CC'].casefold()] = row
+        countries[row['cc'].casefold()] = row
 # print(countries)
 while True:
     chosen = input('please enter country:')
     countryKey = chosen.casefold()
     if countryKey in countries:
         country_data = countries[countryKey]
-        print(f"The capital of {chosen} is {country_data['Capital']}")
+        print(f"The capital of {chosen} is {country_data['capital']}")
     elif chosen == 'quit':
         break
