161ced58a5ed21173b7d21d6244eeb31d77a4902
bryantkrause <bryantkrause@gmail.com>
2022-07-25 14:31:44 -0700

    combine all descriptions into 1 and get invoice date

--- Pdfinator/reImage.py
+++ Pdfinator/reImage.py
@@ -4,10 +4,20 @@ import csv
 import re
 
 from Old import Description, DescriptionEnd
+
+# list of vars
+file = f'.//Files//36908.pdf'
+invoiceList = []
+hourLine = re.compile(r'Hour Meter: ')
+unit = re.compile(r'Unit # ')
+description = re.compile(r'Description Qty Rate Amount')
+descriptionEnd = re.compile(r'Thank you for choosing Gold Standard Service')
+descEnd = ''
+descStart = ''
+invoiceLine = re.compile(r'Invoice ')
+descriptionCombine = []
+descriptinator = ''
 # creating a pdf file object
-
-file = f'.//Files//36907.pdf'
-
 with pdfplumber.open(file) as pdf:
     page = pdf.pages[0]
     text = page.extract_text()
@@ -16,18 +26,15 @@ with pdfplumber.open(file) as pdf:
 # print(text)
 # for x, line in enumerate(text.split('\n')):
 #     print(x, line)
+
+
 # get invoice number
-invoiceLine = re.compile(r'Invoice ')
-
-
 for line in text.split('\n'):
     if invoiceLine.match(line):
         invoiceNumber = line.lstrip("Invoice # ")
         print(f"invoice number is: {invoiceNumber}")
 
 # get meter hours and row number
-hourLine = re.compile(r'Hour Meter: ')
-
 for x, line in enumerate(text.split('\n')):
     if hourLine.match(line):
         rowNum, *hours = line.split()
@@ -35,39 +42,39 @@ for x, line in enumerate(text.split('\n')):
         print(x, f" meter hours are: {meterHours}")
 
 # get unit number
-unit = re.compile(r'Unit # ')
-
 for x, line in enumerate(text.split('\n')):
     if unit.match(line):
         unitNumber = line.lstrip("Unit # ").split(" ", 1)[0]
         print(x, f"unit number is: {unitNumber}")
 
-description = re.compile(r'Description Qty Rate Amount')
 
-descStart = ''
+
+# get start of description line
 for x, line in enumerate(text.split('\n')):
     if description.match(line):
-        print(x,  line)
+        # print(x,  line)
         descStart = x
-print(descStart)
+# print(descStart)
 
-descriptionEnd = re.compile(r'Thank you for choosing Gold Standard Service')
-
-descEnd = ''
+# get end of description
 for x, line in enumerate(text.split('\n')):
     if descriptionEnd.match(line):
-        print(x,  line)
+        # print(x,  line)
         descEnd = x
-print(descEnd)
+# print(descEnd)
 
+# get only description
 for x, line in enumerate(text.split('\n')):
     if descStart < x and x < descEnd:
-        print(x , line)
+        # print(x , line)
+        descriptionCombine.append(line)
+        # print(descriptionCombine)
 
+descriptinator =''.join(descriptionCombine)
+print(descriptinator)
 
-    #     startRow = description.match(line)
-    # if startRow is None:
-    #     startRow = 0
-
-    # if x > 15:
-        
+for x, line in enumerate(text.split('\n')):
+    if x == 2:
+        # print(line)
+        invoiceDate = re.search('([0-9]\/[0-9]\/[0-9]{4})', line).group()
+        print(f"Invoice date is: {invoiceDate}")
