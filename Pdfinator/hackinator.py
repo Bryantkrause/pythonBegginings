c29a3dd67b7b764baf9bfebc293276c497d1eae2
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-08-07 20:03:14 -0700

    hacks

---
+++ Pdfinator/hackinator.py
@@ -0,0 +1,73 @@
+import math
+import os
+import random
+import re
+import sys
+
+#  x = number of shoes
+# n = number of customers who are willing to pay x
+# customers will only pay if they recieved their size
+
+
+# Complete the time_delta function below.
+
+# N = int(raw_input())
+# for i in xrange(N):
+#     print "hello world"
+
+# for i in range(int(input())):
+#     try:
+#         a, b = map(int, input().split())
+#         print(a//b)
+#     except Exception as e:
+#         print("Error Code:", e)
+
+# STDIN = input()
+
+# def is_leap(year):
+#     leap = False
+#     if year % 400 == 0:
+#         leap = True
+#     elif year % 100 == 0:
+#         leap = False
+#     elif year % 4 ==0:
+#         leap = True
+#     else:
+#         leap = False
+#     # Write your logic here
+
+#     return leap
+
+
+# year = int(input())
+# print(is_leap(year))
+
+
+# for i in range(int(input())):
+#     try:
+#         a, b = map(int, input().split())
+#         print(a//b)
+#     except Exception as e:
+#         print("Error Code:", e)
+
+# for i in range(int()):
+#     try:
+#         a,b=map(int, input().split())
+#         print(a//b)
+#     except ZeroDivisionError as e:
+#         print('Error code:', e)
+#     except ValueError as e:
+#         print('Error code:', e)
+
+
+
+# if __name__ == '__main__':
+#     n = int(input().strip())
+#     if n % 2 > 0:
+#         print('Weird')
+#     elif n % 2 == 0 and n < 6:
+#         print('Not Weird')
+#     elif n % 2 == 0 and n <= 20:
+#         print('Weird')
+#     elif n % 2 == 0 and n > 20:
+#         print('Not Weird')
