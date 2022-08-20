503a43798be4573f61118270361195cd5eb09759
bryantkrause <bryantkrause@gmail.com>
2022-08-19 17:05:40 -0700

    the thing

--- Pdfinator/FileLooper.py
+++ Pdfinator/FileLooper.py
@@ -6,7 +6,7 @@ import os
 def getFiles(fileFolder):
     
    # directory = r".//Files"
-   directory = r'\\appserver2k8\finance\2022 Finance\BK\Spendwise\GoldStandard2021'
+   directory = r'\\appserver2k8\finance\2022 Finance\BK\Spendwise\GoldStandardJuneJuly2'
     # iterate over all files in directory
    for filename in os.listdir(directory):
       f = os.path.join(directory, filename)
